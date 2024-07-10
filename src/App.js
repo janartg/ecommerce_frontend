6cb73149ffce1709fb2dbf9cf02ffe5e9b0b2cab
JANARTHANAN V <140438293+janartg@users.noreply.github.com>
2024-07-10 14:33:49 +0530

    frontend file is added

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './pages/home';
+import Header from './components/Header';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    <Home/>
+    <footer/>
+    <footer/>
+    </div>
+  );
+}
+
+export default App;
